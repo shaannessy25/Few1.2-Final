a34ba9a6046c6a18845f6d93c8f231f2f494ec9e
shaannessy25 <47993680+shaannessy25@users.noreply.github.com>
2020-05-14 10:08:54 -0700

    fix deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About'; 
 import Merch from './components/Merch'; 
